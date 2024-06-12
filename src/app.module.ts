2bb968f3f9afc2260771e7d39718afe561939af1
Kyoung Yeob <kkyub1313@gmail.com>
2024-06-12 12:48:31 +0900

    Section10. TypeORM 이론 - Column @ 탐구하기

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserModel,
+    ]),
+    TypeOrmModule.forRoot({
+      // 데이터베이스 타입
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'typeormstudy',
+      entities: [
+        
+      ],
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
