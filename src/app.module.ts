7cda1522fc8470acc1ac0d50cc2a49a4414869ac
Kyoung Yeob <kkyub1313@gmail.com>
2024-06-15 22:15:58 +0900

    Section10. Table Inheritance
    상속 방식은 2가지이니, 주의깊게 체크하세요

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { StudentModel, TeacherModel } from './entity/person.entity';
+import { AirplaneModel, BookModel, CarModel, ComputerModel, SingleBaseModel } from './entity/inheritance.entity';
 
 @Module({
   imports: [
@@ -22,6 +23,11 @@ import { StudentModel, TeacherModel } from './entity/person.entity';
         UserModel,
         StudentModel,
         TeacherModel,
+        BookModel,
+        CarModel,
+        SingleBaseModel,
+        ComputerModel,
+        AirplaneModel
       ],
       synchronize: true,
     })
