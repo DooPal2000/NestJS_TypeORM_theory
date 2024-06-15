7e6cf87cbac68b0e644a4bb912130cdfe683d902
Kyoung Yeob <kkyub1313@gmail.com>
2024-06-15 16:10:59 +0900

    Section10. Column Property 정리하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,12 @@ import { UserModel } from './entity/user.entity';
       // 데이터베이스 타입
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 5433,
       username: 'postgres',
       password: '1234',
       database: 'typeormstudy',
       entities: [
-        
+        UserModel,
       ],
       synchronize: true,
     })
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -4,10 +4,35 @@ import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, Up
 export class UserModel{
     // @PrimaryColumn과 차이점 숙지할 것
     // uuid 는 난수 사용한 고유값 생성, 1씩 올라가는 PrimaryGeneratedColumn 과 차이점 숙지
-    @PrimaryGeneratedColumn('uuid')
+    @PrimaryGeneratedColumn()
     id: number;
     
-    @Column()
+    @Column({
+        type: 'varchar',
+        // 데이터베이스 칼럼 이름
+        // 프로퍼티 이름으로 자동 유추됨
+        name: 'title',
+
+        //값의 길이
+        length: 300,
+
+        // null이 가능한지
+        nullable: true,       
+
+        //true : 처음 저장할때만 값 지정 가능
+        //false: 이후에는 값 변경 불가능
+        update: false,
+
+        //기본값이 true
+        // find() 를 실행시, 기본으로 값을 불러올지
+        select: true,
+
+        //기본 값, 아무것도 입력하지 않았을 때 입력되는 값
+        default: 'default value',
+        
+        // 칼럼중에서 유일무이한 값이 되어야 하는지
+        unique: false,
+    })
     title: string;
 
     // 이건 데이터 생성되는 날짜와 시간 자동으로 찍힘 == timestamp
